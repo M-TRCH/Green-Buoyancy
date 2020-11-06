4cc0d4499a9eab04922a869932eef17fc43e5941
Teerachot Mueangchamnong <61083406+M-TRCH@users.noreply.github.com>
2020-11-07 01:13:53 +0700

    Multi topic publish

--- Green-Buoyancy.ino
+++ Green-Buoyancy.ino
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <wiring_private.h>
+#include <MemoryFree.h>
+#include <pgmStrToRAM.h>
 // Arduino NANO 33 IoT
 #include <TEE_UC20.h>
 #include <internet.h>
@@ -24,16 +26,14 @@ UCxMQTT mqtt;
 Uart mySerial(&sercom0, 5, 6, SERCOM_RX_PAD_1, UART_TX_PAD_0);
 // NANO 33 Hardware serial object
 
-
 void SERCOM0_Handler(); 
 void wake();
 void internet_connect();
 void server_connect();
 String get_value(String data, char separate, int index);
 void sleep();
-int availableMemory(); 
+unsigned long minTomsec(float _min);
 // Prototype functions 
-
 
 void setup()
 {
@@ -43,94 +43,119 @@ void setup()
   Serial.begin(9600); 
   gsm.begin(&mySerial, 9600);
   // Default baudrate
+
+  pinMode(13, OUTPUT);
+  // Set LED pin
 }
 
 
-unsigned long previous_time = -60000*2;
-const long interval_time = 60000*2;
-int count = 0;
+const unsigned long interval_time = minTomsec(30);
+long previous_time = -interval_time;
+float count = 0.0f;
+// interval active
+unsigned long led_prevTime = millis();
+boolean led_state = 1;
+// LED Blink
+unsigned long sleep_prevTime = millis();
+// interval sleep
+
 
 void loop()
 {
  if(millis()-previous_time >= interval_time)
  {
+  previous_time = millis();
   wake(); // UC20 wake up  
 
 
   Serial.print("GPS Starting");
   String gps_data, longitude, latitude = "";
-
-  unsigned long current_time, gps_start = millis();
+  unsigned long gps_time, gps_start = millis();
   do
   {
-   delay(5000);
+   delay(random(3000,5000));
    Serial.print(".");
    gps_data = gps.GetPosition();
    latitude  = get_value(gps_data, ',', 1 );
    longitude = get_value(gps_data, ',', 2 );
-   current_time = millis()-gps_start;
-  } while(latitude == "" && longitude == "" && current_time < 3000); 
+   gps_time = millis()-gps_start;
+  } while(latitude == "" && longitude == "" && gps_time < 3000); 
   Serial.println("\nlatitude : " + latitude);
   Serial.println("longitude : " + longitude);
   Serial.println("GPS Started\n");
   // GPS setup
 
   
-  float         a = float(millis())/1000.0f;
-  uint16_t      b = count/2;
-  long          c = current_time/1000;
-  unsigned long d = random(0, 20);
-  double        e = random(0, 20);
-  float         f = random(0, 20);
-  short         g = random(0, 20);
-  int short     h = random(0, 20);
-  unsigned int  i = random(0, 20);
-  // define variable 
-  String lat = "{\"lat\": 13.8018";
-  String lon = ",\"lon\": 100.5493";
+  float a = float(millis())/60000.0f;
+  float b = count;
+  float c = float(gps_time)/1000.0f;
+  float d = freeMemory();
+  float e = random(1000, 9000)/100.0f;
+  float f = random(1000, 9000)/100.0f;
+  float g = random(1000, 9000)/100.0f;
+  float h = random(1000, 9000)/100.0f;
+  float i = random(1000, 9000)/100.0f;
+  // define variable
+
+  String lat = "{\"lat\":13.8019";
+  String lon = ",\"lon\":100.5548";
   if(latitude != "" && longitude != "")
   {
    lat = "{\"lat\": " + latitude;
    lon = ",\"lon\": " + longitude;
   }
-  String val1 = ",\"V1\": " + String(a, 4);
-  String val2 = ",\"V2\": " + String(b, DEC);
-  String val3 = ",\"V3\": " + String(c, DEC);
-  String val4 = ",\"V4\": " + String(d, DEC);
-  String val5 = ",\"V5\": " + String(e, 1);
-  String val6 = ",\"V6\": " + String(f, 1);
-  String val7 = ",\"V7\": " + String(g, DEC);
-  String val8 = ",\"V8\": " + String(h, DEC);
-  String val9 = ",\"V9\": " + String(i, DEC)+"}";
+  String val1 = ",\"V1\":" + String(a, 2);
+  String val2 = ",\"V2\":" + String(b, 2);
+  String val3 = ",\"V3\":" + String(c, 2);
+  String val4 = ",\"V4\":" + String(d, 2)+"}";
+  String val5 = "{\"V5\":" + String(e, 2);
+  String val6 = ",\"V6\":" + String(f, 2);
+  String val7 = ",\"V7\":" + String(g, 2);
+  String val8 = ",\"V8\":" + String(h, 2);
+  String val9 = ",\"V9\":" + String(i, 2)+"}";
   // convert variable  
 
 
-  String payload = lat + lon + val1 + val2 + val3 
-                       + val4 + val5 + val6
-                       + val7 + val8 + val9;
+  String payload = lat + lon + val1 + val2 + val3 + val4;
+  String payload2 = val5 + val6 + val7 + val8 + val9;
   Serial.println(payload);
+  Serial.println(payload2);
+  Serial.println(payload.length());
+  Serial.println(payload2.length());
   
-  internet_connect();
-  server_connect();
+  internet_connect(); server_connect();
   // 
-  mqtt.Publish("data", payload);
+  mqtt.Publish("data", payload); delay(10);
+  mqtt.Publish("data2", payload2);
+  
+  Serial.println("Published Success");
   // send data
 
+
   sleep(); // UC20 sleep  
-  previous_time = millis();
  }
- Serial.print(count); Serial.print(" ");  
- count += 1;
- delay(30000);
+ count += 0.5; Serial.print(count); Serial.print(" ");  
+ 
+ sleep_prevTime = millis();
+ while(millis()-sleep_prevTime <= minTomsec(0.5))
+ {
+  if(millis()-led_prevTime >= 100)
+  {
+   led_prevTime = millis();
+   digitalWrite(13, led_state);
+   led_state = !led_state;
+  }
+ }
 }
 
+// Function
 void SERCOM0_Handler()
 {
   mySerial.IrqHandler();
 } 
 void wake()
 {
-  gsm.PowerOn();          Serial.println("Power[status]: on");
+  gsm.PowerOn();          Serial.println("\nPower[status]: on");
   while(gsm.WaitReady()); Serial.println("Power[status]: ready");
 
   gps.Start(); Serial.println("GPS[status]: start");
@@ -158,9 +183,9 @@ void internet_connect()
 void server_connect()
 {
   boolean connect = false;
+  Serial.println(F("Server connecting"));
   do
-  {
-     Serial.println(F("Server connecting"));
+  {  
      if(mqtt.DisconnectMQTTServer())
      {
         mqtt.ConnectMQTTServer(mqtt_server, mqtt_port);
@@ -168,12 +193,12 @@ void server_connect()
      connect = mqtt.ConnectState();
      Serial.print("Server connect[state]: ");
      Serial.println(connect);
-     delay(2000);
+     delay(500);
   } while(!connect); 
   
   Serial.println("Server connected");
   unsigned char re_turn = mqtt.Connect(mqtt_ID, mqtt_user, mqtt_password);
-  Serial.println(mqtt.ConnectReturnCode(re_turn)); 
+  //Serial.println(mqtt.ConnectReturnCode(re_turn)); 
   Serial.println();
 }
 String get_value(String data, char separate, int index)
@@ -195,36 +220,12 @@ String get_value(String data, char separate, int index)
 }
 void sleep()
 {
+  mqtt.clear_buffer();
   net.DisConnect(); Serial.println("Server disconnected");
-  delay(1000);
   gsm.PowerOff();   Serial.println("Power[status]: off");
   Serial.println();
 }
-void Publish2(String topic , String payload)
+unsigned long minTomsec(float _min)
 {
-  char chartopic[topic.length()+2];
-  char charpay[payload.length()+2];
-  unsigned char i=0;
-  for(i=0;i<topic.length();i++)
-  {
-    chartopic[i] = topic[i];
-  }
-  chartopic[i]=0;
-  
-  for(i=0;i<payload.length();i++)
-  {
-    charpay[i] = payload[i];
-  }
-  charpay[i]=0;
-  
-  mqtt.Publish(chartopic,topic.length(),charpay,payload.length());
-}
-int availableMemory() 
-{
-    // Use 1024 with ATmega168
-    int size = 2048;
-    byte *buf;
-    while ((buf = (byte *) malloc(--size)) == NULL);
-        free(buf);
-    return size;
+  return _min*60000;  
 }
