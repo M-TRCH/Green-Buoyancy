2bbeaa5dd92eee8b12050073fa5d8a8f4dfc7a99
Teerachot Mueangchamnong <61083406+M-TRCH@users.noreply.github.com>
2020-11-08 10:46:41 +0700

    Add object published
    Add object published (Total 16 obj)

--- Green-Buoyancy.ino
+++ Green-Buoyancy.ino
@@ -33,6 +33,8 @@ void server_connect();
 String get_value(String data, char separate, int index);
 void sleep();
 unsigned long minTomsec(float _min);
+void data_package(String lat, String lon, float V1,  float V2,  float V3,  float V4,  float V5,  float V6,  float V7,
+                                float V8, float V9, float V10, float V11, float V12, float V13, float V14, String *pay);
 // Prototype functions 
 
 void setup()
@@ -73,7 +75,7 @@ void loop()
   unsigned long gps_time, gps_start = millis();
   do
   {
-   delay(random(3000,5000));
+   delay(random(3000, 9000));
    Serial.print(".");
    gps_data = gps.GetPosition();
    latitude  = get_value(gps_data, ',', 1 );
@@ -86,58 +88,48 @@ void loop()
   // GPS setup
 
   
-  float a = float(millis())/60000.0f;
-  float b = count;
-  float c = float(gps_time)/1000.0f;
-  float d = freeMemory();
-  float e = random(1000, 9000)/100.0f;
-  float f = random(1000, 9000)/100.0f;
-  float g = random(1000, 9000)/100.0f;
-  float h = random(1000, 9000)/100.0f;
-  float i = random(1000, 9000)/100.0f;
+  String lat = "13.8019";
+  String lon = "100.5548"; // โรงจอดรถไฟฟ้า BTS
+  if(latitude != "" && longitude != ""){ lat = latitude; lon = longitude; }  
+  float V1 = float(millis())/60000.0f;
+  float V2 = count;
+  float V3 = float(gps_time)/1000.0f;
+  float V4 = freeMemory();
+  float V5  = random(1000,  3000)/100.0f;
+  float V6  = random(   0,  1400)/100.0f;
+  float V7  = random(   0, 50000)/100.0f;
+  float V8  = random(   0,   150)/100.0f;
+  float V9  = random(   0,  2200)/100.0f;
+  float V10 = random(   0,  1000)/100.0f;
+  float V11 = random(   0,  3000)/100.0f;
+  float V12 = random(   0,  1000)/100.0f;
+  float V13 = random( 800,  1600)/100.0f;
+  float V14 = random(   0,  1000)/100.0f;
   // define variable
 
-  String lat = "{\"lat\":13.8019";
-  String lon = ",\"lon\":100.5548";
-  if(latitude != "" && longitude != "")
-  {
-   lat = "{\"lat\": " + latitude;
-   lon = ",\"lon\": " + longitude;
-  }
-  String val1 = ",\"V1\":" + String(a, 2);
-  String val2 = ",\"V2\":" + String(b, 2);
-  String val3 = ",\"V3\":" + String(c, 2);
-  String val4 = ",\"V4\":" + String(d, 2)+"}";
-  String val5 = "{\"V5\":" + String(e, 2);
-  String val6 = ",\"V6\":" + String(f, 2);
-  String val7 = ",\"V7\":" + String(g, 2);
-  String val8 = ",\"V8\":" + String(h, 2);
-  String val9 = ",\"V9\":" + String(i, 2)+"}";
-  // convert variable  
+
+  String payload[2];
+  data_package(lat, lon, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, payload);
+  Serial.println("payload(0)[info]: "  + payload[0]); Serial.println(payload[0].length());
+  Serial.println("payload(1)[info]: "  + payload[1]); Serial.println(payload[1].length());
+  // data compile
 
 
-  String payload = lat + lon + val1 + val2 + val3 + val4;
-  String payload2 = val5 + val6 + val7 + val8 + val9;
-  Serial.println(payload);
-  Serial.println(payload2);
-  Serial.println(payload.length());
-  Serial.println(payload2.length());
-  
   internet_connect(); server_connect();
   // 
-  mqtt.Publish("data", payload); delay(10);
-  mqtt.Publish("data2", payload2);
-  
+  mqtt.Publish("data" , payload[0]); delay(10);
+  mqtt.Publish("data2", payload[1]);
   Serial.println("Published Success");
   // send data
 
-
   sleep(); // UC20 sleep  
  }
- count += 0.5; Serial.print(count); Serial.print(" ");  
+
+ 
+ count += 0.1; Serial.print(count); Serial.print(" ");  
  
  sleep_prevTime = millis();
- while(millis()-sleep_prevTime <= minTomsec(0.5))
+ while(millis()-sleep_prevTime <= minTomsec(0.1))
  {
   if(millis()-led_prevTime >= 100)
   {
@@ -145,7 +137,7 @@ void loop()
    digitalWrite(13, led_state);
    led_state = !led_state;
   }
- }
+ } // bedtime
 }
 
 // Function
@@ -228,4 +220,30 @@ void sleep()
 unsigned long minTomsec(float _min)
 {
   return _min*60000;  
+}
+void data_package(String lat, String lon, float V1,  float V2,  float V3,  float V4,  float V5,  float V6,  float V7,
+                                float V8, float V9, float V10, float V11, float V12, float V13, float V14, String *pay)
+{
+  uint8_t DP = 2; // Ex. 9.47
+
+  lat = "{\"lat\": " + lat;
+  lon = ",\"lon\": " + lon;
+  String _V1  = ",\"V1\":"  + String(V1,  DP);
+  String _V2  = ",\"V2\":"  + String(V2,  DP);
+  String _V3  = ",\"V3\":"  + String(V3,  DP);
+  String _V4  = ",\"V4\":"  + String(V4,  DP);
+  String _V5  = ",\"V5\":"  + String(V5,  DP);
+  String _V6  = ",\"V6\":"  + String(V6,  DP)+"}";
+  String _V7  = "{\"V7\":"  + String(V7,  DP);
+  String _V8  = ",\"V8\":"  + String(V8,  DP);
+  String _V9  = ",\"V9\":"  + String(V9,  DP);
+  String _V10 = ",\"V10\":" + String(V10, DP);
+  String _V11 = ",\"V11\":" + String(V11, DP);
+  String _V12 = ",\"V12\":" + String(V12, DP);
+  String _V13 = ",\"V13\":" + String(V13, DP);
+  String _V14 = ",\"V14\":" + String(V14, DP)+"}";
+  // convert variable  
+  
+  pay[0] = lat + lon + _V1 + _V2 + _V3 + _V4 + _V5 + _V6;
+  pay[1] = _V7 + _V8 + _V9 + _V10 + _V11 + _V12 + _V13 + _V14;
 }
